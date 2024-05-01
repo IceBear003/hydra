22a52169d94c027f21500969bbd4ffc22a0a3dfb
IceBear003 <1294243258@qq.com>
2024-05-01 16:15:18 +0800

    16:15

--- v3.0/sram_interface.sv
+++ v3.0/sram_interface.sv
@@ -1,15 +1,23 @@
-`include "fifo_null_pages.sv"
+`include "./v3.0/fifo_null_pages.sv"
 
-module sram_state(
+module sram_state
+#(
+    parameter ECC_STORAGE_ADDR_WIDTH = 11,
+    parameter ECC_STORAGE_DATA_WIDTH = 8,
+    parameter ECC_STORAGE_DATA_DEPTH = 2048
+)
+(
     input clk,
     input rst_n,
  
-    input wr_ecc_en,
-    input [10:0] wr_ecc_addr,
-    input [7:0] wr_ecc_code,
-    
-    input [10:0] rd_ecc_addr,
-    output reg [7:0] rd_ecc_code = 0,
+    //ECC Storage
+    input ecc_wr_en,
+    input [10:0] ecc_wr_addr,
+    input [7:0] ecc_din,
+
+    input ecc_rd_en,
+    input [10:0] ecc_rd_addr,
+    output reg [7:0] ecc_dout = 0,
 
     input wr_op,
     input [3:0] wr_port,
@@ -17,13 +25,34 @@ module sram_state(
     input [3:0] rd_port,
     input [10:0] rd_addr,
 
+    //SRAM State
     output reg writting = 0,
-    output reg reading = 0,
-    output reg full = 0,
 
+    //LOCK
+    input reg lock_en,
+    input reg lock_dis,
+    output reg locking = 0,
+
+    //TODO READ MECHANISM
+
+    //Null Pages
     output [10:0] null_ptr,
-
     output reg [10:0] free_space = 2047,
+    output reg full = 0
 );
+
+reg [ECC_STORAGE_DATA_WIDTH-1:0] ecc_storage [ECC_STORAGE_DATA_DEPTH-1:0];
+
+always @(posedge clk) begin
+    if(ecc_wr_en && rst_n) begin 
+        ecc_storage[ecc_wr_addr] <= ecc_din;
+    end
+end
+
+always @(posedge clk) begin
+    if(ecc_rd_en && rst_n) begin
+        ecc_dout <= ecc_storage[ecc_rd_addr];
+    end
+end
 
 endmodule
