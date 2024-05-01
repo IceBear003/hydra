f67af26308514bf9523d6bca1755466e2d48513d
IceBear003 <1294243258@qq.com>
2024-05-01 14:31:23 +0800

    update 14:31

--- v3.0/fifo_null_pages.sv
+++ v3.0/fifo_null_pages.sv
@@ -11,29 +11,30 @@ module fifo_null_pages
 );
 
 reg [10:0] fifo [2047:0];
-reg [10:0] head_ptr = 0;
+reg [10:0] head_ptr = 1;
 reg [10:0] tail_ptr = 0;
 reg initialized = 1;
 
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
-        head_ptr = 0;
-        tail_ptr = 0;
-        head_addr = 0;
-        fifo[0] = 0;
-    end
-
-    if(pop_head && rst_n)
-        if(initialized && head_addr < 2047) 
-            head_addr = head_addr + 1;
-        else begin
-            initialized = 0;
-            head_ptr = head_ptr + 1;
-            head_addr = fifo[head_ptr];
+        head_ptr <= 1;
+        tail_ptr <= 0;
+        head_addr <= 0;
+        fifo[0] <= 0;
+        initialized <= 1;
+    end else begin
+        if(pop_head) 
+            if(initialized && head_addr < 2047) 
+                head_addr <= head_addr + 1;
+            else begin
+                initialized <= 0;
+                head_ptr <= head_ptr + 1;
+                head_addr <= fifo[head_ptr + 1];
+            end
+        if(push_tail) begin
+            fifo[tail_ptr + 1] <= tail_addr;
+            tail_ptr <= tail_ptr + 1;
         end
-    if(push_tail && rst_n) begin
-        tail_ptr = tail_ptr + 1;
-        fifo[tail_ptr] = tail_addr;
     end
 end
 
