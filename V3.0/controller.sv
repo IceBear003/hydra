0ffbd121684702058a635a618b757f3016b7cf00
ExBritainia <2602539804@qq.com>
2024-05-03 03:12:22 +0800

    2024.5.3 3:12

--- V3.0/controller.sv
+++ V3.0/controller.sv
@@ -18,21 +18,22 @@ integer i, j;
 reg [15:0][4:0] distribution;//ç«¯å£å¯¹åº”çš„SRAM
 reg [31:0][3:0] bind_dest_port;//SRAMå¯¹åº”çš„ç«¯å?
 
-always @(negedge rst_n) begin
-    for(i = 0; i < 16; i = i + 1) begin
-        distribution[i] <= i;
-    end
-end
-
 reg [15:0] search_tag = 0;
 reg [15:0][10:0] max_amount = 0;//ä¸´æ—¶æœ?å¤§å??
 reg [31:0]locking = 0;//SRAM lock
 reg [15:0][2:0]batch = 0;// Data page
 reg [15:0][8:0]left = 0;//æŒä¹…åŒ–çš„æ•°æ®é•¿åº¦
-reg [15:0][3:0]dest_port_;//æŒä¹…åŒ–çš„dest port
+reg [15:0][3:0]dest_port_ = 0;//æŒä¹…åŒ–çš„dest port
 reg [15:0][2:0]prior_;
-reg [15:0][10:0]page;// port page writein
+reg [15:0][10:0]page = 0;// port page writein
 reg [15:0]end_;
+
+always @(negedge rst_n) begin
+    for(i = 0; i < 16; i = i + 1) begin
+        distribution[i] <= i;
+        locking[i] <= 1;
+    end
+end
 
 wire [15:0] [2:0] port_prior;
 wire [15:0] [3:0] port_dest_port;
@@ -53,7 +54,7 @@ wire [31:0][7:0] ecc_dout;
 
 reg [31:0]wr_op;
 reg [31:0][3:0] wr_port;
-reg [31:0]rd_op;
+reg [31:0]rd_op = 0;
 reg [31:0][3:0] rd_port;
 reg [31:0][10:0] rd_addr;
 
@@ -83,8 +84,11 @@ reg [31:0][15:0] sram_din;
 reg [31:0] sram_rd_en;
 reg [31:0][13:0] sram_rd_addr;
 wire [31:0][15:0] sram_dout;
+wire [15:0] xfer_en;
+reg [15:0] xfer_s = 0;
 
 always @(posedge clk) begin
+    if(rst_n == 1) begin
     for(i = 0; i < 16; i = i + 1) begin
         if(port_writting[i]) begin
             if(port_new_packet[i]) begin
@@ -92,20 +96,36 @@ always @(posedge clk) begin
                     free_space[distribution[i]] < port_length[i]) begin
                     search_tag[i] <= 1;//æ›´æ¢å†™å…¥çš„SRAM
                     request_port[(cnt+i)%32 + 1] <= port_dest_port[i];
+                    $display("port_dest_port[i] = %d, i = %d",port_dest_port[i],i);
                 end
             end
         end
-
+        $display("cnt = %d",cnt);
+        //$display("port_new_packet[i] = %d", port_new_packet[i]);
+        //$display("port_writting[i] = %d", port_writting[i]);
+        //$display("dest_port_[i] = %d", dest_port_[i]);
+        //$display("port_dest_port[i] = %d",port_dest_port[i]);
+        //$display("left[i] = %d", left[i]);
+        //$display("port_data_vld[i] = %d",port_data_vld[i]);
+        //$display("port_length[i] = %d",port_length[i]);
         if(left[i] == 0) begin
             end_[i] <= 0;
         end
 
+        if(xfer_en[i] == 1 && port_data_vld[i] == 0) begin
+            page[i] <= null_ptr[distribution[i]];
+            search_tag[i] <= 0;
+            wr_op[distribution[i]] <= 1;
+            wr_port[distribution[i]] <= dest_port_[i];
+            $display("       distribution[i] = %d, i = %d",distribution[i],i);
+        end
         if(port_data_vld[i]) begin//ç«¯å£æ­£åœ¨è¾“å‡ºdata
             if(left[i] == 0) begin//è¾“å‡ºdataçš„ç¬¬ä¸?ä¸ªtickå’Œè¾“å…¥dataçš„æœ€åŽä¸€ä¸ªtickåº”è¯¥éƒ½éœ€è¦ï¼Œæœ‰ç‚¹æ··ä¹±
                 left[i] <= port_length[i];
                 dest_port_[i] <= port_dest_port[i];
                 prior_[i] <= port_prior[i];
-                search_tag[i] <= 0;
+                wr_op[distribution[i]] <= 0;
+                //search_tag[i] <= 0;
             end else begin 
                 if(left[i] == 1) begin
                     end_[i] <= 1;
@@ -114,6 +134,8 @@ always @(posedge clk) begin
             end
             if(batch[i] == 7 && !end_[i]) begin//åœ¨æ•°æ®æœ«çš„æ—¶å€™ï¼Ÿåœ¨æ•°æ®åˆçš„æ—¶å€?,åº”å½“éœ?è¦é¢„å…ˆä¸€ä¸ªtickï¼ˆåœ¨æœç´¢ç»“æŸçš„æ—¶å€™ï¼‰å¤„ç†page
                 page[i] <= null_ptr[distribution[i]];
+                $display("distribution[i] = %d",distribution[i]);
+                $display("     null_ptr[distribution[i]] = %d",null_ptr[distribution[i]]);
                 wr_op[distribution[i]] <= 1;//SRAMæ­£åœ¨å†™å…¥
                 wr_port[distribution[i]] <= dest_port_[i];//å†™å…¥çš„ç«¯å?
 
@@ -139,14 +161,15 @@ always @(posedge clk) begin
             ecc_encoder_data[i] <= (port_data[i] << (batch[i] << 4)) + ecc_encoder_data[i];
         end
         
-        if(search_tag[i]) begin
+        if(search_tag[i] && xfer_en[i] == 0) begin
             if(locking[(cnt+i)%32] != 1) begin
-                if(page_amount[(cnt+i)%32] > max_amount[i]) begin//ä¸?ä¸ªSRAMä¸­æœ‰å¤šå°‘ä¸ªdest_portçš„æ•°æ?
+                if(page_amount[(cnt+i)%32] >= max_amount[i]) begin//ä¸?ä¸ªSRAMä¸­æœ‰å¤šå°‘ä¸ªdest_portçš„æ•°æ?
                     locking[(cnt+i)%32] <= 1; 
                     locking[distribution[i]] <= 0;
                     distribution[i] <= (cnt+i)%32;
                     //max amountè¦è¿˜åŽ?
                     max_amount[i] <= page_amount[(cnt+i)%32];
+                    $display("distribution[i] = %d, i = %d",distribution[i],i,page_amount[(cnt+i)%32],max_amount[i]);
                     bind_dest_port[(cnt+i)%32] <= port_dest_port[i];
                 end
                 request_port[(cnt+i)%32 + 1] <= port_dest_port[i];
@@ -154,6 +177,7 @@ always @(posedge clk) begin
         end
     end
     cnt <= cnt + 1;
+    end
 end
 
 always @(negedge rst_n) begin
@@ -177,7 +201,8 @@ port port [15:0]
     .data(port_data),
     .length(port_length),
     .writting(port_writting),
-    .new_packet(port_new_packet)
+    .new_packet(port_new_packet),
+    .xfer_en(xfer_en)
     // .unlock(port_unlock)
 );
 
--- V3.0/port.sv
+++ V3.0/port.sv
@@ -11,34 +11,34 @@ module port(
     output reg [2:0] prior = 3'b0,
     output reg [3:0] dest_port = 4'b0,
     //前32拍延迟32拍发出，后续拍数据的延迟情况取决于vld
+    output reg data_vld = 0,
     output reg [15:0] data = 16'b0,
     output reg [8:0] length = 0,
     output reg writting = 0,
-    output reg unlock = 0
+    output reg new_packet = 0,
+    output reg xfer_en = 0
 );
 
 reg is_ctrl_frame = 0;
 
-//数据包缓冲区 32×16
-reg [31:0][15:0] buffer = 0;
+//数据包缓冲区 33×16
+reg [32:0][15:0] buffer = 0;
 
 //多数据包数据缓冲管理
 //兼容32周期的搜索时序
-reg xfer_en = 0;
-reg [4:0] xfer_ptr = 0;
-reg [4:0] write_ptr = 0;
-reg [4:0] last_ptr = 0;
-
-//为突发传输准备
-reg [3:0] heartbeat = 0;
+reg [5:0] xfer_ptr = 0;
+reg [5:0] write_ptr = 0;
+reg [5:0] last_ptr = 0;
  
 always @(posedge clk) begin 
     if(wr_sop) begin
+        new_packet <= 1;
         is_ctrl_frame <= 1;
     end
     if(wr_eop) begin
         prior <= 3'b0;
         dest_port <= 4'b0;
+        data_vld <= 0;
         data <= 16'b0;
         last_ptr <= write_ptr;
     end
@@ -46,7 +46,8 @@ always @(posedge clk) begin
 
     if(xfer_en) begin
         data <= buffer[xfer_ptr];
-        if(xfer_ptr + 1 == write_ptr || (xfer_ptr == 31 && write_ptr == 0)) begin
+        data_vld <= 1;
+        if(xfer_ptr + 1 == write_ptr || (xfer_ptr == 32 && write_ptr == 0)) begin//
             xfer_en <= 0;
         end
         if((xfer_ptr == last_ptr) && xfer_stop) begin
@@ -57,32 +58,36 @@ always @(posedge clk) begin
         //$display("xfer_stop = %d",xfer_stop);
         //$display("data = %d",data);
         xfer_ptr <= xfer_ptr + 1;
+    end else begin
+        data_vld <= 0;
     end
-
+    if(xfer_ptr == 32) begin
+        xfer_ptr <= 0;
+    end
+    if(write_ptr == 32) begin
+        write_ptr <= 0;
+    end
     if(wr_vld) begin
-        unlock <= 1;
-        heartbeat <= 0;
         if(is_ctrl_frame) begin
             dest_port <= wr_data[3:0];
             prior <= wr_data[6:4];
             length <= wr_data[15:7];
+            $display("dest_port = %d, prior = %d, length = %d",dest_port,prior,length);
+
             buffer[write_ptr] <= {7'b0, wr_data[15:7]};
             is_ctrl_frame <= 0;
         end
         else begin
+            new_packet <= 0;
             buffer[write_ptr] <= wr_data;
         end
-        if(write_ptr + 1 == xfer_ptr || (write_ptr == 31 && xfer_ptr == 0)) begin
+        //$display("write_ptr = %d",write_ptr);
+        //$display("xfer_ptr = %d",xfer_ptr);
+        if(write_ptr + 1 == xfer_ptr || (write_ptr == 32 && xfer_ptr == 0)) begin//
             xfer_en <= 1;
         end
         write_ptr <= write_ptr + 1;
         //$display("         write_ptr = %d",write_ptr);
-    end else begin
-        if(heartbeat == 15) begin
-            heartbeat <= 0;
-            unlock <= 1;
-        end
-        heartbeat <= heartbeat + 1;
     end
 end
 
