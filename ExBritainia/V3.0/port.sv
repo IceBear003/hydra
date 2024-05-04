687e883a2cfb46e003de16fc726ec7fc395b213b
ExBritainia <2602539804@qq.com>
2024-05-04 21:38:21 +0800

    2024.5.4 21:38

--- ExBritainia/V3.0/port.sv
+++ ExBritainia/V3.0/port.sv
@@ -13,7 +13,7 @@ module port(
     output reg [8:0] length = 0,
     output reg data_vld = 0,
     output reg [15:0] data = 16'b0,
-    output reg xfer_en = 0,
+    output reg start_of_packet = 0,
 
     //Use to trigger the update of the persistent dest_port & length in controller.sv
     output reg new_packet = 0
@@ -24,6 +24,7 @@ reg [63:0][15:0] buffer = 0;
 reg delay_cnt_en = 0;
 reg [5:0] delay_cnt = 0;
 
+reg xfer_en = 0;
 reg [5:0] xfer_ptr = 0;
 
 reg is_ctrl_frame = 0;
@@ -42,16 +43,27 @@ end
 always @(posedge clk) begin
     if(delay_cnt == 32) begin
         xfer_en <= 1;
+        //start_of_packet <= 1;
     end else if (xfer_ptr == end_ptr) begin
         xfer_en <= 0;
+        //start_of_packet <= 0;
         end_ptr <= 1'bx;
         $display("     end_ptr = %d",end_ptr);
     end else if (wr_eop && xfer_ptr == wr_ptr - 1) begin
         xfer_en <= 0;
+        //start_of_packet <= 0;
         end_ptr <= 1'bx;
     end
     $display("  xfer_ptr = %d",xfer_ptr);
     $display("     end_ptr = %d",end_ptr);
+end
+
+always @(posedge clk) begin
+    if(delay_cnt == 32) begin
+        start_of_packet <= 1;
+    end else begin
+        start_of_packet <= 0;
+    end
 end
 
 always @(posedge clk) begin
@@ -112,6 +124,7 @@ always @(posedge clk) begin
         $display("delay_cnt = %d",delay_cnt);
     end else begin
         data_vld <= 0;
+        //start_of_packet <= 0;
     end
 end
 
