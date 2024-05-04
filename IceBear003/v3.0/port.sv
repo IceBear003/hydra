07091e53545c3aca791c35c0491ce4660d214bbf
IceBear003 <1294243258@qq.com>
2024-05-04 19:07:16 +0800

    19:07

--- IceBear003/v3.0/port.sv
+++ IceBear003/v3.0/port.sv
@@ -9,6 +9,7 @@ module port(
     input xfer_stop,
 
     output reg [3:0] dest_port = 4'b0,
+    output reg [2:0] prior = 0,
     output reg [8:0] length = 0,
     output reg data_vld = 0,
     output reg [15:0] data = 16'b0,
@@ -23,10 +24,10 @@ reg delay_cnt_en = 0;
 reg [5:0] delay_cnt = 0;
 
 reg xfer_en = 0;
-reg [5:0] xfer_ptr;
+reg [5:0] xfer_ptr = 0;
 
 reg is_ctrl_frame = 0;
-reg [5:0] wr_ptr;
+reg [5:0] wr_ptr = 0;
 reg [5:0] end_ptr;
 
 always @(posedge clk) begin
@@ -42,12 +43,13 @@ always @(posedge clk) begin
         xfer_en <= 1;
     end else if (xfer_ptr == end_ptr) begin
         xfer_en <= 0;
+        end_ptr <= 1'bx;
     end
 end
 
 always @(posedge clk) begin
     if(wr_eop) begin
-        end_ptr <= wr_ptr;
+        end_ptr <= wr_ptr - 1;
     end
 end
 
@@ -55,18 +57,29 @@ always @(posedge clk) begin
     if(wr_vld) begin
         buffer[wr_ptr] <= wr_data;
         wr_ptr <= wr_ptr + 1;
+        $display("wr_ptr = %d",wr_ptr);
+        $display("        wr_data = %d",wr_data);
     end
 end
 
 always @(posedge clk) begin
-    is_ctrl_frame <= wr_sop;
+    //is_ctrl_frame <= wr_sop;
+    //$display("            is_ctrl_frame = %d",is_ctrl_frame);
+    //$display("wr_vld = %d",wr_vld);
+    //$display("wr_sop = %d",wr_sop);
+    if(wr_sop)
+        is_ctrl_frame <= 1;
+    else if(wr_vld)
+        is_ctrl_frame <= 0;
 end
 
 always @(posedge clk) begin
-    if(is_ctrl_frame) begin
+    if(is_ctrl_frame && wr_vld) begin
         dest_port <= wr_data[3:0];
+        prior <= wr_data[6:4];
         length <= wr_data[15:7];
         new_packet <= 1;
+        $display("wr_data = %d",wr_data);
     end else begin
         new_packet <= 0;
     end
@@ -82,7 +95,7 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-    if(xfer_en) begin
+    if(xfer_en && xfer_ptr != wr_ptr) begin
         data <= buffer[xfer_ptr];
         data_vld <= 1;
         xfer_ptr <= xfer_ptr + 1;
