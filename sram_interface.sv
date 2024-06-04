b51bf0540d91c1308e0beaf3de73bb5411e016d1
IceBear003 <1294243258@qq.com>
2024-06-05 06:37:00 +0800

    2024.6.5 6:36

--- sram_interface.sv
+++ sram_interface.sv
@@ -1,5 +1,6 @@
 `include "sram.sv"
 `include "sram_ecc_encoder.sv"
+`include "sram_ecc_decoder.sv"
 
 module sram_interface
 #(parameter SRAM_IDX = 0)
@@ -7,14 +8,18 @@ module sram_interface
     input clk,
     input rst_n,
 
-    //需要32组MUX16-1
-    input wr_wr_xfer_data_vld,
+    input wr_xfer_data_vld,
     input [15:0] wr_xfer_data,
     input wr_end_of_packet,
-
-    //包的首尾页地址，无需多选器
     output reg [15:0] wr_packet_head_addr,
     output reg [15:0] wr_packet_tail_addr,
+
+    input [3:0] rd_port,
+    output rd_xfer_data_vld,
+    output [15:0] rd_xfer_data,
+    output rd_end_of_packet,
+    input [15:0] rd_packet_head_addr,
+    output reg [15:0] rd_next_packet_head_addr,
 
     input [3:0] check_port,
     output [8:0] check_amount,
@@ -22,48 +27,68 @@ module sram_interface
 );
 
 // 0 - idle
-// 1 - packet writting the first page
-// 2 - packet writting the left pages
-reg [1:0] state;
+// 1 - writting the first page
+// 2 - writting the left pages
+reg [1:0] wr_state;
 reg [2:0] wr_batch;
 
-reg [8:0] packet_amount [15:0];
-assign check_amount = packet_amount[check_port];
-
-integer port;
 always @(posedge clk) begin
     if(!rst_n) begin
-        free_space <= 11'd2047;
-        for(port = 0; port < 16; port = port + 1) begin
-            packet_amount[port] <= 0;
-        end
-    end else if(state == 2'd0 && wr_wr_xfer_data_vld) begin
-        free_space <= free_space - wr_xfer_data[15:7];
-        packet_amount[wr_xfer_data[3:0]] <= packet_amount[wr_xfer_data[3:0]] + 1;
+        wr_state <= 2'd0;
+    end else if(wr_state == 2'd0 && wr_xfer_data_vld) begin
+        wr_state <= 2'd1;
+    end else if(wr_state == 2'd1 && wr_batch == 3'd7 && wr_xfer_data_vld) begin
+        wr_state <= 2'd2;
+    end else if(wr_state == 2'd2 && wr_end_of_packet) begin
+        wr_state <= 2'd0;
     end
 end
 
 always @(posedge clk) begin
     if(!rst_n) begin
-        state <= 2'd0;
-    end else if(state == 2'd0 && wr_wr_xfer_data_vld) begin
-        state <= 2'd1;
-    end else if(state == 2'd1 && wr_batch == 3'd7 && wr_wr_xfer_data_vld) begin
-        state <= 2'd2;
-    end else if(state == 2'd2 && wr_end_of_packet) begin
-        state <= 2'd0;
-    end
-end
-
-always @(posedge clk) begin
-    if(!rst_n || wr_end_of_packet) begin
         wr_batch <= 0;
-    end else if(wr_wr_xfer_data_vld) begin
+    end else if(wr_xfer_data_vld) begin
         wr_batch <= wr_batch + 1;
+    end else if(wr_end_of_packet) begin
+        wr_batch <= 0;
     end
 end
 
-//空闲链表
+//Per round(page)
+reg [2:0] rd_batch;
+
+//Multi-port concurrent read status
+// 0 - idle
+// 1 - reading the first page(length not got)
+// 2 - reading the left pages(length got)
+reg [1:0] rd_state [15:0];
+reg [10:0] rd_page [15:0];
+reg [5:0] rd_length [15:0];
+
+always @(posedge clk) begin
+    if(!rst_n) begin
+        
+    end else if(rd_length[rd_port] == 0) begin
+        
+    end else begin
+        
+    end
+end
+
+always @(posedge clk) begin
+    if(!rst_n) begin
+        rd_batch <= 0;
+    end else if(rd_length[rd_port] == 0) begin
+        rd_batch <= 0;
+    end else begin
+        rd_batch <= rd_batch + 1;
+    end
+end
+
+/*
+ * Sub-module "null_pages": Recycling and reallocating data pages.
+ */
+
 (* ram_style = "block" *) reg [10:0] null_pages [2047:0];
 reg np_init;
 reg [10:0] np_head;
@@ -73,7 +98,7 @@ reg [10:0] np_top;
 always @(posedge clk) begin
     if(!rst_n) begin
         np_tail <= 0;
-    end /*else if(rd_batch == 7) begin
+    end /*else if(rd_batch[port] == 7) begin
         null_pages[np_tail] <= rd_addr;
         np_tail <= np_tail + 1;
     end */
@@ -84,50 +109,96 @@ always @(posedge clk) begin
         np_head <= 0;
         np_top <= 2047;
         np_init <= 1;
-    end if(wr_batch != 7) begin
+    end else if(wr_batch != 7 && ~wr_end_of_packet) begin
     end else if(np_init == 1 & np_top != 0) begin
         np_top <= np_top - 1;
     end else begin
-        np_init <= 0;
-        np_top <= null_pages[np_head + 1];
+        np_top <= null_pages[np_head];
         np_head <= np_head + 1;
+        np_init <= 0;
     end
 end
 
-//ECC
-(* ram_style = "block" *) reg [7:0] ecc_storage [2047:0];
-reg [10:0] es_wr_addr;
-reg [7:0] es_din;
+/*
+ * Sub-module "ECC_manager": encode data written into SRAM;
+ *                           decode data read from SRAM.
+ */
 
-reg [15:0] ecc_buffer [7:0];
-wire [7:0] ecc_result;
+(* ram_style = "block" *) reg [7:0] ecc_codes [2047:0];
+
+reg [10:0] ec_wr_addr;
+reg [7:0] ec_din;
+reg [15:0] ecc_wr_buffer [7:0];
+wire [7:0] ecc_wr_result;
 
 always @(posedge clk) begin
-    if(wr_wr_xfer_data_vld == 0) begin
+    if(~wr_xfer_data_vld) begin
     end else if(wr_batch == 3'd0) begin
-        ecc_buffer[0] <= wr_xfer_data;
-        ecc_buffer[1] <= 0;
-        ecc_buffer[2] <= 0;
-        ecc_buffer[3] <= 0;
-        ecc_buffer[4] <= 0;
-        ecc_buffer[5] <= 0;
-        ecc_buffer[6] <= 0;
-        ecc_buffer[7] <= 0;
+        ecc_wr_buffer[0] <= wr_xfer_data;
+        ecc_wr_buffer[1] <= 0;
+        ecc_wr_buffer[2] <= 0;
+        ecc_wr_buffer[3] <= 0;
+        ecc_wr_buffer[4] <= 0;
+        ecc_wr_buffer[5] <= 0;
+        ecc_wr_buffer[6] <= 0;
+        ecc_wr_buffer[7] <= 0;
     end else begin
-        ecc_buffer[wr_batch] <= wr_xfer_data;
+        ecc_wr_buffer[wr_batch] <= wr_xfer_data;
     end
 end
 
 always @(posedge clk) begin
     if(wr_batch == 7 || wr_end_of_packet) begin
-        es_wr_addr <= np_top;
-        es_din <= ecc_result;
+        ec_wr_addr <= np_top;
+        ec_din <= ecc_wr_result;
     end
 end
 
 always @(posedge clk) begin
-    ecc_storage[es_wr_addr] <= es_din;
+    ecc_codes[ec_wr_addr] <= ec_din;
 end
+
+always @(posedge clk) begin
+    ec_dout <= ecc_codes[ec_rd_addr];
+end
+
+sram_ecc_encoder sram_ecc_encoder( 
+    .data_0(ecc_wr_buffer[0]),
+    .data_1(ecc_wr_buffer[1]),
+    .data_2(ecc_wr_buffer[2]),
+    .data_3(ecc_wr_buffer[3]),
+    .data_4(ecc_wr_buffer[4]),
+    .data_5(ecc_wr_buffer[5]),
+    .data_6(ecc_wr_buffer[6]),
+    .data_7(ecc_wr_buffer[7]),
+    .code(ecc_wr_result)
+);
+
+reg [10:0] ec_rd_addr;
+reg [7:0] ec_dout;
+reg [15:0] ecc_rd_buffer [7:0];
+reg [15:0] ecc_rd_data [7:0];
+
+sram_ecc_decoder sram_ecc_decoder(
+    .update,
+    .data_0(ecc_rd_buffer[0]),
+    .data_1(ecc_rd_buffer[1]),
+    .data_2(ecc_rd_buffer[2]),
+    .data_3(ecc_rd_buffer[3]),
+    .data_4(ecc_rd_buffer[4]),
+    .data_5(ecc_rd_buffer[5]),
+    .data_6(ecc_rd_buffer[7]),
+    .data_7(ecc_rd_buffer[6]),
+    .code(ec_dout),
+    .cr_data_0(ecc_rd_data[0]),
+    .cr_data_1(ecc_rd_data[1]),
+    .cr_data_2(ecc_rd_data[2]),
+    .cr_data_3(ecc_rd_data[3]),
+    .cr_data_4(ecc_rd_data[4]),
+    .cr_data_5(ecc_rd_data[5]),
+    .cr_data_6(ecc_rd_data[6]),
+    .cr_data_7(ecc_rd_data[7])
+);
 
 //跳转表
 (* ram_style = "block" *) reg [15:0] jump_table [2047:0];
@@ -140,9 +211,9 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-    if(state == 2'd1) begin
+    if(wr_state == 2'd1) begin
         wr_packet_head_addr <= np_top;
-    end else if(state != 2'd2) begin
+    end else if(wr_state != 2'd2) begin
     end else if(wr_end_of_packet == 0) begin
         jump_table[jt_wr_addr] <= np_top;
     end else begin
@@ -150,27 +221,31 @@ always @(posedge clk) begin
     end
 end
 
+reg [8:0] packet_amount [15:0];
+assign check_amount = packet_amount[check_port];
+
+integer port;
+always @(posedge clk) begin
+    if(!rst_n) begin
+        free_space <= 11'd2047;
+        for(port = 0; port < 16; port = port + 1) begin
+            packet_amount[port] <= 0;
+        end
+    end else if(wr_state == 2'd0 && wr_xfer_data_vld) begin
+        free_space <= free_space - wr_xfer_data[15:7];
+        packet_amount[wr_xfer_data[3:0]] <= packet_amount[wr_xfer_data[3:0]] + 1;
+    end
+end
+
 sram sram(
     .clk(clk),
     .rst_n(rst_n),
-    .wr_en(wr_wr_xfer_data_vld),
+    .wr_en(wr_xfer_data_vld),
     .wr_addr({np_top, wr_batch}),
     .din(wr_xfer_data)/*,
     .rd_en(),
     .rd_addr(),
     .dout()*/
-);
-
-sram_ecc_encoder sram_ecc_encoder( 
-    .data_0(ecc_buffer[0]),
-    .data_1(ecc_buffer[1]),
-    .data_2(ecc_buffer[2]),
-    .data_3(ecc_buffer[3]),
-    .data_4(ecc_buffer[4]),
-    .data_5(ecc_buffer[5]),
-    .data_6(ecc_buffer[6]),
-    .data_7(ecc_buffer[7]),
-    .code(ecc_result)
 );
 
 endmodule
